1469540faea6c2f553dff122462e17f8d88a5230
mkreive <mkreive@gmail.com>
2022-06-13 12:07:17 +0300

    header-search component created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,13 +10,25 @@ const Header = function () {
                     <div className='theme-switch'>
                         <div className='theme-switch__sun'></div>
                         <div className='theme-switch__bar'>
-                            <span className='theme-switch__bar-circle'></span>
+                            <div className='theme-switch__bar-circle'></div>
                         </div>
                         <div className='theme-switch__moon'></div>
                     </div>
                 </div>
             </div>
-            <div className='header-search'></div>
+            <div className='header-search'>
+                <input
+                    type='text'
+                    className='input input-title'
+                    placeholder='Filter by title, companies, expertise...'
+                />
+                <input type='text' className='input input-location' placeholder='Filter by location...' />
+                <div>
+                    <div className='checkbox'></div>
+                    <div className='text-black'></div>
+                    <button className='btn'></button>
+                </div>
+            </div>
         </div>
     );
 };
