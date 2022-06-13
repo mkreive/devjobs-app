44cedf2e0b88adcbd53f518ddd4f04c99fef3dea
mkreive <mkreive@gmail.com>
2022-06-13 11:37:47 +0300

    header-top component done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import '../index.scss';
 
 const Header = function () {
-    return <div className='app-header'></div>;
+    return (
+        <div className='header'>
+            <div className='header-top'>
+                <div className='header-top__container'>
+                    <img className='logo'></img>
+                    <div className='theme-switch'>
+                        <div className='theme-switch__sun'></div>
+                        <div className='theme-switch__bar'>
+                            <span className='theme-switch__bar-circle'></span>
+                        </div>
+                        <div className='theme-switch__moon'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='header-search'></div>
+        </div>
+    );
 };
 
 export default Header;
