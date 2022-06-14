d22fc9f51a4860ca2a423e89a95503fa868c8c83
mkreive <mkreive@gmail.com>
2022-06-14 11:22:01 +0300

    main responsive done

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -4,13 +4,13 @@ import '../index.scss';
 const JobCard = function (props) {
     return (
         <div className='card'>
-            <div className='card__logo'></div>
+            <span className='card__logo'></span>
             <div className='card__text-group'>
                 <div className='text-gray'>5h ago</div>
-                <div className='text-gray'>&#8226;</div>
+                <div className='text-gray text-dot'>&#8226;</div>
                 <div className='text-gray'>Full Time</div>
             </div>
-            <div className='text-bold'>Senior Software engineer</div>
+            <div className='text-bold-big'>Senior Software engineer</div>
             <div className='text-gray'>Scoot</div>
             <div className='text-purple'>United Kingdom</div>
         </div>
