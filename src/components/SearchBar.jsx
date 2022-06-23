547daf2b14483b33e66b8325869961fae5eaea2a
mkreive <mkreive@gmail.com>
2022-06-23 15:40:32 +0300

    input filtering started

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,23 +11,48 @@ const SearchBar = function () {
     const fullTimeHandler = function () {
         if (!fullTimeChecked) {
             setFullTimeChecked(url);
-            dispatch({ type: 'contract' });
+            dispatch({ type: 'contract', time: 'full' });
         } else {
             setFullTimeChecked('');
             dispatch({ type: 'all' });
         }
     };
 
+    const locationInputHandler = function () {
+        const inputValue = 'United Kingdom';
+        dispatch({ type: 'location', input: inputValue });
+    };
+
+    const titleInputHandler = function () {
+        const inputValue = 'Junior Full-Stack Developer';
+        dispatch({ type: 'input', input: inputValue });
+    };
+    const searchHandler = function () {
+        console.log('search btn clicked');
+    };
+
     return (
         <div className='header-search'>
-            <input type='text' className='input input-title' placeholder='Filter by title, companies, expertise...' />
+            <input
+                type='text'
+                className='input input-title'
+                placeholder='Filter by title, companies, expertise...'
+                onClick={titleInputHandler}
+            />
             <hr className='line' />
-            <input type='text' className='input input-location' placeholder='Filter by location...' />
+            <input
+                type='text'
+                className='input input-location'
+                placeholder='Filter by location...'
+                onClick={locationInputHandler}
+            />
             <hr className='line' />
             <div className='input-submit'>
                 <div className='checkbox' onClick={fullTimeHandler} style={{ backgroundImage: fullTimeChecked }}></div>
                 <div className='text text-bold'>Full time only</div>
-                <button className='btn'>Search</button>
+                <button className='btn' onClick={searchHandler}>
+                    Search
+                </button>
             </div>
         </div>
     );
--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,18 @@ import data from '../data.json';
 
 const filtersReducer = function (state = data, action) {
     if (action.type === 'contract') {
-        return data.filter((job) => job.contract === 'Full Time');
+        if (action.time === 'full') return data.filter((job) => job.contract === 'Full Time');
+        if (action.time === 'part') return data.filter((job) => job.contract === 'Part Time');
+        if (action.time === 'freelance') return data.filter((job) => job.contract === 'Freelance');
+        return data;
     }
     if (action.type === 'location') {
         return data.filter((job) => job.location === 'United Kingdom');
     }
     if (action.type === 'input') {
-        return data.filter((job) => job.position === 'Senior Software Engineer');
+        const inputValue = action.input;
+
+        return data.filter((job) => job.position === inputValue || job.company === inputValue);
     }
     if (action.type === 'all') {
         return data;
