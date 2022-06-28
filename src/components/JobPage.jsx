41a2b23ebc8a938bffa763f477ebe349ca12f848
mkreive <mkreive@gmail.com>
2022-06-28 14:40:10 +0300

    job page done

--- src/components/JobPage.jsx
+++ src/components/JobPage.jsx
@@ -8,9 +8,12 @@ const JobPage = function (props) {
     return (
         <div className='main-job'>
             <div className='banner'>
-                <div className='banner-logo'></div>
+                <div
+                    className='banner-logo'
+                    style={{ backgroundImage: `url(${company.logo})`, backgroundColor: `${company.logoBackground}` }}
+                ></div>
                 <div className='banner__info-company'>
-                    <h2 className='text-title-big'>{company.company}</h2>
+                    <h2 className='text-title-big'>{company.position}</h2>
                     <div className='text-gray-big'>{company.company}</div>
                 </div>
                 <a href={company.website} className='btn btn-light'>
@@ -28,7 +31,9 @@ const JobPage = function (props) {
                         <h1 className='text-title-very-big'>{company.position}</h1>
                         <div className='text-purple-nomargin'>{company.location}</div>
                     </div>
-                    <button className='btn'>Apply Now</button>
+                    <a href={company.apply} className='btn'>
+                        Apply Now
+                    </a>
                 </div>
                 <div className='job__body'>
                     <p className='text-gray-big text-block'>{company.description}</p>
@@ -36,23 +41,27 @@ const JobPage = function (props) {
                     <div className='text-gray-big text-block'>
                         {company.requirements.content}
                         <div className='text-empty-line'></div>
-                        {company.requirements.items.forEach((item) => {
-                            <div className='text-list'>
-                                <span className='text-star'>*</span>
-                                <span className='text-gray-big'>{item}</span>
-                            </div>;
-                        })}
+                        {company.requirements.items &&
+                            company.requirements.items.map((item) => {
+                                return (
+                                    <div className='text-list' key={Math.random() * 100}>
+                                        <span className='text-star'>*</span>
+                                        <span className='text-gray-big'>{item}</span>
+                                    </div>
+                                );
+                            })}
                     </div>
                     <h2 className='text-title-big'>What You Will Do</h2>
                     <div className='text-block text-gray-big'>
                         {company.role.content}
                         <div className='text-empty-line'></div>
-                        {company.role.items.forEach((item) => {
-                            <div className='text-list'>
-                                <span className='text-star'>1</span>
-                                <span className='text-gray-big'>{item} </span>
-                            </div>;
-                        })}
+                        {company.role.items &&
+                            company.role.items.map((item) => (
+                                <div className='text-list' key={company.role.items.indexOf(item) + 1}>
+                                    <span className='text-star'>{company.role.items.indexOf(item) + 1}</span>
+                                    <span className='text-gray-big'>{item} </span>
+                                </div>
+                            ))}
                     </div>
                 </div>
             </div>
@@ -62,7 +71,9 @@ const JobPage = function (props) {
                         <h2 className='text-title-big'>{company.position}</h2>
                         <div className='text-gray-big'>{company.company}</div>
                     </div>
-                    <button className='btn'>Apply Now</button>
+                    <a href={company.apply} className='btn'>
+                        Apply Now
+                    </a>
                 </div>
             </footer>
         </div>
